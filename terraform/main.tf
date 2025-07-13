e35a1b7189fd89511c6ab8eb2c3e8964e7ab496d
Manisha Gupta <43117629+Manisha26Gupta@users.noreply.github.com>
2025-07-13 17:45:30 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.75.0"
+    }
+  }
+
+  required_version = ">= 1.3.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-aks-flux"
+  location = "West Europe"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-flux-cluster"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "fluxcluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    Environment = "GitOps"
+  }
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
